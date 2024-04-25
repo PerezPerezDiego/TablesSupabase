0be8286c72eb7f63d0de594099db782de5226f2f
PerezPerezDiego <diegop35689@gmail.com>
2024-04-25 12:41:46 -0600

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import NavigationMenu from './NavigationMenu';
+
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient("https://vqkdzggmcljkqewgwtwa.supabase.co", 
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxa2R6Z2dtY2xqa3Fld2d3dHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzMTc0NDYsImV4cCI6MjAyODg5MzQ0Nn0.pjgi6N4W4jDw87KhJq13EPtyiMuzmrjVn5TtrWRsTEY");
+  
+      
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [productos, setProductos] = useState([]);
+
+  useEffect(()=>{
+    getProductos();
+  },[]);
+
+  async function getProductos(){
+    const {data} = await supabase.from("productos").select();
+    setProductos(data);
+  }
+    return (
+      
+        <div className="App">
+          <ul>
+        {productos.map((productos)=>(
+        <li key={productos.descripcion}>{productos.descripcion}</li>
+        ))}
+      </ul>
+            <NavigationMenu />
+            {/* Aquí va el resto de tu aplicación */}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/NavigationMenu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Menu() {
+    return (
+        <ul>
+            <li><a href="usuarios/usuarios.html">Usuarios</a></li>
+            <li><a href="sesionesProductos/sesionesProductos.html">Sesiones de Productos</a></li>
+            <li><a href="sesiones/sesiones.html">Sesiones</a></li>
+            <li><a href="productos/productos.html">Productos</a></li>
+            <li><a href="genero/genero.html">Géneros</a></li>
+            <li><a href="direccion/direccion.html">Dirección</a></li>
+            <li><a href="clientes/clientes.html">Clientes</a></li>
+            <li><a href="categorias/categorias.html">Categorías</a></li>
+        </ul>
+    );
+}
+
+export default Menu;
